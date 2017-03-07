af3aec2a6c5472740ad3f61b3ad6a8cf0f7bae10
Nayan Konaje <nayan@uber.com>
2017-03-06 23:51:24 -0800

    Add controller

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -9,6 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var tip: UILabel!
+    @IBOutlet weak var total: UILabel!
+    @IBOutlet weak var bill: UITextField!
+    
+    @IBOutlet weak var control: UISegmentedControl!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +26,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+   
+    @IBAction func calculateTip(_ sender: AnyObject) {
+        
+        let tipPercentages = [0.18, 0.20, 0.25]
+        
+        let billAmount = Double(bill.text!) ?? 0
+        let tipAmount = billAmount * tipPercentages[control.selectedSegmentIndex]
+        let totalAmount = billAmount + tipAmount
+        
+        tip.text = String(format: "$%.2f", tipAmount)
+        total.text = String(format: "$%.2f", totalAmount)
+        
+    }
 }
 
